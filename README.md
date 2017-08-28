b671050246e55d25f8ea2dbaa9d7de3e9c1410ca
andefined <dimitris.papaevagelou@andefined.com>
2017-08-28 18:30:08 +0300

    instructions

--- README.md
+++ README.md
@@ -3,15 +3,33 @@
 [![Go Releaser](https://img.shields.io/github/release/andefined/twreport.svg)](https://goreportcard.com/report/github.com/andefined/twreport/releases/latest)
 [![Code Coverage](https://img.shields.io/codecov/c/github/andefined/twreport/master.svg)](https://codecov.io/gh/andefined/twreport/releases/latest)
 
+[![twreport](screenshot.png)](https://github.com/andefined/twreport)
+
 # twreport
+A simple CLI for batch reporting / blocking abusive accounts on Twitter.
+
+## Installation
+You can download the binaries from the [releases](/releases) section, or you can install it with Go.
+
+```bash
+go get -u github.com/andefined/twreport
+go install github.com/andefined/twreport
+```
+
+## How to use
+1. Prepare you *csv* file without a header. Just point the correct column of the *screen_name* with `--column` flag. Default column is *0*.
+
+    e.g. CSV `874276197357596672,realDonaldTrump`
+2. Create an [application](https://apps.twitter.com) and generate *Access Token and Token Secret* under *Keys and Access Tokens* tab.
+
 
 ```bash
 twreport report \
-    --consumer-key key \
-    --consumer-secret secret \
-    --access-token token \
-    --access-token-secret token-secret \
-    --csv /path/to/block.csv \
+    --consumer-key $CONSUMER-KEY \
+    --consumer-secret $CONSUMER-SECRET \
+    --access-token $ACCESS-TOKEN \
+    --access-token-secret $ACCESS-TOKEN-SECRET \
+    --csv ./templates/block.csv \
     --column 1 \
     --prompt \
     --block
--- twreport.go
+++ twreport.go
@@ -18,13 +18,13 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "twreport"
-	app.Version = "0.0.1"
-	app.Usage = "Twitter: Report / Block Users"
+	app.Version = "0.0.2"
+	app.Usage = "Batch Report / Block Abusive Accounts on Twitter"
 
 	app.Commands = []cli.Command{
 		{
 			Name:   "report",
-			Usage:  "Report / Block Users from File",
+			Usage:  "Batch Report / Block Abusive Accounts from a csv file",
 			Action: Report,
 			Flags: []cli.Flag{
 				cli.StringFlag{
@@ -69,32 +69,30 @@ func main() {
 // Report : Report Command
 func Report(c *cli.Context) error {
 	now := time.Now()
+	// Read the flags
 	project := &Project{}
-
 	project.CSV = c.String("csv")
 	project.Column = c.Int("column")
 	project.Block = c.BoolT("block")
 	project.Prompt = c.BoolT("prompt")
-
 	project.ConsumerKey = c.String("consumer-key")
 	project.ConsumerSecret = c.String("consumer-secret")
 	project.AccessToken = c.String("access-token")
 	project.AccessTokenSecret = c.String("access-token-secret")
 
-	if project.ConsumerKey == "" || project.ConsumerSecret == "" || project.AccessToken == "" || project.AccessTokenSecret == "" {
-		log.Fatal("Consumer key/secret and Access token/secret required")
-	}
-
+	// Twitter API
 	anaconda.SetConsumerKey(project.ConsumerKey)
 	anaconda.SetConsumerSecret(project.ConsumerSecret)
 	api := anaconda.NewTwitterApi(project.AccessToken, project.AccessTokenSecret)
-	verify, err := api.VerifyCredentials()
 
-	if err != nil || !verify {
+	// Verify Twitter Credentials
+	_, err := api.VerifyCredentials()
+	if err != nil {
 		fmt.Printf("%s. Please refer to %s for your Access Tokens.\n", "Bad Authorization Tokens", "https://apps.twitter.com/")
 		return nil
 	}
 
+	// Open the Input File
 	file, err := os.Open(project.CSV)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -102,21 +100,26 @@ func Report(c *cli.Context) error {
 	}
 	defer file.Close()
 
+	// Read the Input File
 	reader := csv.NewReader(file)
 	reader.Comma = ','
 
+	// Create an Output File
+	// This is usefull for keeping a log of users that you allready reported/blocked
 	output, err := os.OpenFile("./twreport-"+strconv.FormatInt(now.Unix(), 6)+".csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return nil
 	}
-
 	defer output.Close()
 
+	// Output File Header
 	writer := csv.NewWriter(output)
 	writer.Write([]string{"screen_name", "report", "block"})
 	writer.Flush()
+
 	for {
+		// Read Line By Line
 		record, err := reader.Read()
 		if err == io.EOF {
 			break
@@ -134,7 +137,7 @@ func Report(c *cli.Context) error {
 						_, err := api.PostUsersReportSpam(field, v)
 						if err != nil {
 							fmt.Printf("You are over the limit for spam reports. Sleeping for 15'.\n")
-							sleep()
+							Sleep()
 						}
 						fmt.Printf("User %s Reported (Blocked: %v).\n", field, project.Block)
 						writer.Write([]string{field, "true", strconv.FormatBool(project.Block)})
@@ -144,7 +147,7 @@ func Report(c *cli.Context) error {
 					_, err := api.PostUsersReportSpam(field, v)
 					if err != nil {
 						fmt.Printf("You are over the limit for spam reports. Sleeping for 15'.\n")
-						sleep()
+						Sleep()
 					}
 					fmt.Printf("User %s Reported (Blocked: %v).\n", field, project.Block)
 					writer.Write([]string{field, "true", strconv.FormatBool(project.Block)})
@@ -169,7 +172,7 @@ type Project struct {
 	Prompt            bool   `yaml:"prompt"`
 }
 
-// PromptConfirm : ...
+// PromptConfirm : Prompt for Confirmation
 func PromptConfirm() bool {
 	var response string
 	_, err := fmt.Scanln(&response)
@@ -182,10 +185,11 @@ func PromptConfirm() bool {
 	if strings.ToLower(string(rune(response[0]))) == "n" {
 		return false
 	}
-	fmt.Printf("Please type yes or no and then press enter: ")
+	fmt.Printf("Please type y (for yes) or n (for no) and then press enter: ")
 	return PromptConfirm()
 }
 
-func sleep() {
+// Sleep : Go to Sleep for 15 Minutes (API Limits)
+func Sleep() {
 	time.Sleep(15 * time.Minute)
 }
